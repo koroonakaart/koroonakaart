f91f97508f0d5ab5faa7f0c9368ce4e8455f2c11
keeganmcbride <keegan.mcbride@outlook.com>
2020-03-15 17:51:44 +0200

    debugging

--- javascript/visualisation.js
+++ javascript/visualisation.js
@@ -41,6 +41,7 @@ function drawCounties() {
             // Debug
             console.log(d);
             prevalence_data.set(d.id, +d['confirmed']);
+            test_data.set(d.id, +d['confirmed','active','in_treatment','deaths','recovered','population'])
         })
         .await(ready);
 
@@ -59,6 +60,7 @@ function drawCounties() {
         console.log('prevalence_data:');
         console.log(prevalence_data);
         console.log('');
+        console.log(test_data);
 
         // Load population data
         var counties = topojson.feature(data, {
