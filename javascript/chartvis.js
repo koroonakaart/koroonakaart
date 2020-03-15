0ce89ce08ddb0a93aa4d6731b2ff41fd08637a8e
Chris Thompson <info@neuroactive.ai>
2020-03-15 05:29:48 +0200

    Adjust legend position in daily cases chart

--- javascript/chartvis.js
+++ javascript/chartvis.js
@@ -312,8 +312,8 @@ function casesPerDay() {
         },
         
         legend: {
-            x: 0.01,
-            y: 0.99,
+            x: 0.05,
+            y: 0.90,
             bgcolor: '#FFF',
             opacity: 0.7,
             bordercolor: '#000',
