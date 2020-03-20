e884db013be3545713913e6073cd2b4c80efe4bb
keeganmcbride <keegan.mcbride@outlook.com>
2020-03-20 16:54:12 +0200

    Add CSV / XLSX export

--- koroonakaart/src/main.js
+++ koroonakaart/src/main.js
@@ -9,6 +9,7 @@ import VueAnalytics from "vue-analytics";
 import exportingInit from "highcharts/modules/exporting";
 import stockInit from "highcharts/modules/stock";
 import offlineExporting from "highcharts/modules/offline-exporting";
+import exportData from "highcharts/modules/export-data";
 
 import mapInit from "highcharts/modules/map";
 
@@ -22,6 +23,7 @@ mapInit(Highcharts);
 stockInit(Highcharts);
 exportingInit(Highcharts);
 offlineExporting(Highcharts);
+exportData(Highcharts);
 
 Vue.use(HighchartsVue);
 
