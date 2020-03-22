c62e4afe0314785a727b8766cc15a8dd74385cba
Harry Sild <puhhbaer@hotmail.com>
2020-03-22 19:15:19 +0200

    add direct routes for i18n versions

--- koroonakaart/src/router/index.js
+++ koroonakaart/src/router/index.js
@@ -4,16 +4,23 @@ import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
+/* const defaultLocale = this.$i18n.locale; */
+
 const routes = [
   {
     path: "/",
+    redirect: "/ee" /* `/${defaultLocale}` */
+  },
+  {
+    path: "/:locale",
     name: "Home",
     component: Home
   }
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: "history"
 });
 
 export default router;
