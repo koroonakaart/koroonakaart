bbcb6f85695318965caae129ab6c6c19a92ebc55
Harry Sild <puhhbaer@hotmail.com>
2020-03-26 12:34:48 +0200

    more localisation fixes

--- koroonakaart/src/components/charts/TestsAgeSexDistributionChart.vue
+++ koroonakaart/src/components/charts/TestsAgeSexDistributionChart.vue
@@ -9,6 +9,10 @@ import data from "../../data.json";
 
 export default {
   name: "TestsAgeSexDistributionChart",
+  mounted() {
+    console.log(this.chartOptions);
+  },
+
   data() {
     return {
       chartOptions: {
@@ -17,46 +21,82 @@ export default {
           align: "left",
           y: 30
         },
+
         chart: {
           type: "bar",
           height: 470
         },
+
         navigation: {
           buttonOptions: {
             verticalAlign: "top",
             y: -15
           }
         },
+
         // Remove Highcharts.com link from bottom right
         credits: {
           enabled: false
         },
 
-        xAxis: [{
-        categories: ["0 - 4", "5 - 9", "10 - 14", "15 - 19", "20 - 24", "25 - 29", "30 - 34", "35 - 39", "40 - 44", "45 - 49", "50 - 54", "55 - 59", "60 - 64", "65+"],
-          reversed: false,
-          labels: {
-             step:1
-          }},
+        xAxis: [
+          {
+            categories: [
+              "0 - 4",
+              "5 - 9",
+              "10 - 14",
+              "15 - 19",
+              "20 - 24",
+              "25 - 29",
+              "30 - 34",
+              "35 - 39",
+              "40 - 44",
+              "45 - 49",
+              "50 - 54",
+              "55 - 59",
+              "60 - 64",
+              "65+"
+            ],
+            reversed: false,
+            labels: {
+              step: 1
+            }
+          },
           {
             opposite: true,
             reversed: false,
-            categories: ["0 - 4", "5 - 9", "10 - 14", "15 - 19", "20 - 24", "25 - 29", "30 - 34", "35 - 39", "40 - 44", "45 - 49", "50 - 54", "55 - 59", "60 - 64", "65+"],
+            categories: [
+              "0 - 4",
+              "5 - 9",
+              "10 - 14",
+              "15 - 19",
+              "20 - 24",
+              "25 - 29",
+              "30 - 34",
+              "35 - 39",
+              "40 - 44",
+              "45 - 49",
+              "50 - 54",
+              "55 - 59",
+              "60 - 64",
+              "65+"
+            ],
             linkedTo: 0,
             labels: {
-                step: 1
-            },
-        }],
+              step: 1
+            }
+          }
+        ],
 
         yAxis: {
           title: {
             text: this.$t("numberOfTests")
           },
-    labels: {
-      formatter: function () {
-        return Math.abs(this.value);
-			}
-      }
+          labels: {
+            formatter: function() {
+              return Math.abs(this.value);
+            }
+          }
         },
         plotOptions: {
           bar: {
@@ -66,25 +106,34 @@ export default {
         },
 
         tooltip: {
-            formatter: function () {
-                return '<b>' + this.series.name + '  ' + this.point.category + '</b><br/>' +
-                    + Math.abs(this.point.y);
-            }
+          formatter: function() {
+            return (
+              "<b>" +
+              this.series.name +
+              "  " +
+              this.point.category +
+              "</b><br/>" +
+              +Math.abs(this.point.y)
+            );
+          }
         },
 
-
         series: [
-	{
-    name: this.$t("maleNegative"),
-            data: data.dataPositiveTestsByAgeChart.maleNegative.map(x => x * (-1)),
+          {
+            name: this.$t("maleNegative"),
+            data: data.dataPositiveTestsByAgeChart.maleNegative.map(
+              x => x * -1
+            ),
             color: "#97beeb"
           },
           {
             name: this.$t("malePositive"),
-            data: data.dataPositiveTestsByAgeChart.malePositive.map(x => x * (-1)),
+            data: data.dataPositiveTestsByAgeChart.malePositive.map(
+              x => x * -1
+            ),
             color: "#2f7ed8"
           },
-           {
+          {
             name: this.$t("femaleNegative"),
             data: data.dataPositiveTestsByAgeChart.femaleNegative,
             color: "#917ea9"
@@ -93,8 +142,8 @@ export default {
             name: this.$t("femalePositive"),
             data: data.dataPositiveTestsByAgeChart.femalePositive,
             color: "#492970"
-          },
-         ]
+          }
+        ]
       }
     };
   },
@@ -110,13 +159,12 @@ export default {
   watch: {
     currentLocale() {
       this.chartOptions.title.text = this.$t("distributionOfAgeSexTests");
-      this.chartOptions.xAxis.title.text = this.$t("age");
       this.chartOptions.yAxis.title.text = this.$t("numberOfTests");
       this.chartOptions.series[0].name = this.$t("maleNegative");
       this.chartOptions.series[1].name = this.$t("malePositive");
       this.chartOptions.series[2].name = this.$t("femaleNegative");
       this.chartOptions.series[3].name = this.$t("femalePositive");
-  }
+    }
   }
 };
 </script>
