41163378a3d90ecf71f351f948dc2f822c081f7e
Harry Sild <puhhbaer@hotmail.com>
2020-03-29 15:27:40 +0300

    add log and ln buttons to DailyCountyCasesChart

--- koroonakaart/src/components/charts/DailyCountyCasesChart.vue
+++ koroonakaart/src/components/charts/DailyCountyCasesChart.vue
@@ -13,22 +13,104 @@ export default {
   data() {
     return {
       chartOptions: {
+        chartType: "linear",
+
         title: {
           text: this.$t("confirmedCasesByCounties"),
           align: "left",
           y: 30
         },
 
+        chart: {
+          height: 470,
+          events: {
+            load: function() {
+              // Buttons have indexes go in even numbers (button1 [0], button2 [2])
+              // Odd indexes are button symbols
+              const button = this.exportSVGElements[4];
+
+              // States:
+              // 0 - normal
+              // 1 - hover
+              // 2 - selected
+              // 3 - disabled
+              button.setState(2);
+            },
+            redraw: function() {
+              // Redraw seems to be async so setTimeout for the button to update state
+              setTimeout(() => {
+                this.exportSVGElements[4].setState(
+                  this.options.chartType === "linear" ? 2 : 0
+                );
+                this.exportSVGElements[2].setState(
+                  this.options.chartType === "logarithmic" ? 2 : 0
+                );
+              }, 100);
+            }
+          }
+        },
+
+        exporting: {
+          buttons: {
+            customButton2: {
+              text: this.$t("logarithmic"),
+              onclick: function() {
+                this.options.chartType = "logarithmic";
+
+                this.yAxis[0].update({
+                  type: "logarithmic"
+                });
+              }
+            },
+            customButton: {
+              text: this.$t("linear"),
+              onclick: function() {
+                this.options.chartType = "linear";
+
+                this.yAxis[0].update({
+                  type: "linear"
+                });
+              }
+            }
+          }
+        },
+
         // Remove Highcharts.com link from bottom right
         credits: {
           enabled: false
         },
+
         navigation: {
           buttonOptions: {
             verticalAlign: "top",
-            y: -15
+            y: -15,
+            theme: {
+              fill: "none",
+              stroke: "none",
+              "stroke-width": 0,
+              r: 4,
+
+              states: {
+                hover: {
+                  /* fill: "#f5f5f5" */
+                },
+                select: {
+                  fill: "none",
+                  style: {
+                    fontWeight: "bold",
+                    textDecoration: "underline"
+                  }
+                }
+              },
+              style: {
+                /* color: "#039", */
+                /* fontWeight: "bold", */
+                textDecoration: "none"
+              }
+            }
           }
         },
+
         xAxis: {
           categories: data.dates1,
           crosshair: true,
@@ -47,7 +129,8 @@ export default {
         },
 
         yAxis: {
-          min: 0,
+          /* min: 0, */
+          allowNegativeLog: true,
           title: {
             text: this.$t("numberOfCases")
           }
@@ -149,6 +232,10 @@ export default {
       this.chartOptions.title.text = this.$t("confirmedCasesByCounties");
       this.chartOptions.yAxis.title.text = this.$t("numberOfCases");
       this.chartOptions.xAxis.plotLines[0].label.text = this.$t("method");
+      this.chartOptions.exporting.buttons.customButton.text = this.$t("linear");
+      this.chartOptions.exporting.buttons.customButton2.text = this.$t(
+        "logarithmic"
+      );
     }
   }
 };
