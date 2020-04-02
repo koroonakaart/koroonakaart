2c2d51c883aff9426abe55fe07c32bd244af1292
keeganmcbride <keegan.mcbride@outlook.com>
2020-04-02 11:06:15 +0300

    update data hide charts.

--- koroonakaart/src/views/Home.vue
+++ koroonakaart/src/views/Home.vue
@@ -2,9 +2,10 @@
 <template>
   <b-container fluid="lg">
     <Statsbar />
-    <b-row class="mb-5">
+    <SuveChatbot />
+
+    <!--<b-row class="mb-5">
       <Map />
-      <SuveChatbot />
     </b-row>
 
     <b-row class="mb-5">
@@ -14,7 +15,7 @@
       <b-col cols="12" lg="6">
         <TestsPopRatioChart />
       </b-col>
-    </b-row>
+    </b-row>-->
     <!-- <b-row class="mb-5">
       <CumulativeCasesChart />
     </b-row>
@@ -43,34 +44,36 @@
 
 <script>
 import Statsbar from "../components/Statsbar";
-import Map from "../components/Map";
-
 import CumulativeTestsChart from "../components/charts/CumulativeTestsChart";
-/* import CumulativeCasesChart from "../components/charts/CumulativeCasesChart";
+
+/*import Map from "../components/Map";
+
+ import CumulativeCasesChart from "../components/charts/CumulativeCasesChart";
 import NewCasesPerDayChart from "../components/charts/NewCasesPerDayChart";
 import TestsPerDayChart from "../components/charts/TestsPerDayChart";
 import PositiveTestsAgeDistributionChart from "../components/charts/PositiveTestsAgeDistributionChart";
-import DailyCountyCasesChart from "../components/charts/DailyCountyCasesChart"; */
+import DailyCountyCasesChart from "../components/charts/DailyCountyCasesChart";
 // import TestsAgeSexDistributionChart from "../components/charts/TestsAgeSexDistributionChart";
 import ConfirmedCasesByCountiesChart from "../components/charts/ConfirmedCasesByCountiesChart";
 // import PositiveNegativeChart from "../components/charts/PositiveNegativeChart";
-import TestsPopRatioChart from "../components/charts/TestsPopRatioChart";
+import TestsPopRatioChart from "../components/charts/TestsPopRatioChart";*/
 
 export default {
   name: "Home",
   // Register components you want to use in the page here
   components: {
     Statsbar,
-    Map,
     CumulativeTestsChart,
-    /* CumulativeCasesChart,
+
+  /*   Map,
+    CumulativeCasesChart,
     NewCasesPerDayChart,
     TestsPerDayChart,
     PositiveTestsAgeDistributionChart,
-    DailyCountyCasesChart, */
+    DailyCountyCasesChart,
     ConfirmedCasesByCountiesChart,
     // PositiveNegativeChart,
-    TestsPopRatioChart,
+    TestsPopRatioChart,*/
     SuveChatbot: () => import("../components/SuveChatbot")
   }
 };
