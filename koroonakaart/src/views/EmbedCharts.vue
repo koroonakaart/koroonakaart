d129a5c53698abe4380f2ce765e41c5b9acb0bcd
Harry Sild <puhhbaer@hotmail.com>
2020-04-03 10:31:51 +0300

    CumulativeTestsChart fix

---
+++ koroonakaart/src/views/EmbedCharts.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <component :is="chartToDisplay" />
+  </div>
+</template>
+
+<script>
+/* eslint-disable vue/no-unused-components */
+export default {
+  name: "EmbedCharts",
+
+  components: {
+    Map: () => import("../components/Map"),
+    CumulativeTestsChart: () =>
+      import("../components/charts/CumulativeTestsChart"),
+    CumulativeCasesChart: () =>
+      import("../components/charts/CumulativeCasesChart"),
+    NewCasesPerDayChart: () =>
+      import("../components/charts/NewCasesPerDayChart"),
+    TestsPerDayChart: () => import("../components/charts/TestsPerDayChart"),
+    TestsAgeSexDistributionChart: () =>
+      import("../components/charts/TestsAgeSexDistributionChart"),
+    DailyCountyCasesChart: () =>
+      import("../components/charts/DailyCountyCasesChart"),
+    ConfirmedCasesByCountiesChart: () =>
+      import("../components/charts/ConfirmedCasesByCountiesChart"),
+    PositiveNegativeChart: () =>
+      import("../components/charts/PositiveNegativeChart"),
+    TestsPopRatioChart: () => import("../components/charts/TestsPopRatioChart")
+    //  PositiveTestsAgeDistributionChart:() => import("../components/charts/PositiveTestsAgeDistributionChart.vue"),
+  },
+
+  mounted() {
+    console.log(this.$route.query);
+  },
+
+  data() {
+    return {
+      chartToDisplay: this.$route.query.chart
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
