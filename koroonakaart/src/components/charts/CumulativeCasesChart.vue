2854e2ceda8de8f30ccaa168ec33c42d6f12ae1b
keeganmcbride <keegan.mcbride@outlook.com>
2020-05-04 11:12:30 +0300

    update chart tooltip

--- koroonakaart/src/components/charts/CumulativeCasesChart.vue
+++ koroonakaart/src/components/charts/CumulativeCasesChart.vue
@@ -174,7 +174,16 @@ export default {
             text: this.$t("numberOfCases")
           }
         },
-
+        tooltip: {
+          headerFormat:
+            '<span style="font-size:10px">{point.key}</span><table>',
+          pointFormat:
+            '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y}</b></td></tr>',
+          footerFormat: "</table>",
+          shared: true,
+          useHTML: true
+        },
         series: [
           {
             name: this.$t("confirmedCases"),
