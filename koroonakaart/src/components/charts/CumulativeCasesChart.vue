5dbe7659abac281646615204471723da1f30c57f
Harry Sild <puhhbaer@hotmail.com>
2020-06-08 12:46:07 +0300

    remove legend when screen is too small

--- koroonakaart/src/components/charts/CumulativeCasesChart.vue
+++ koroonakaart/src/components/charts/CumulativeCasesChart.vue
@@ -126,9 +126,11 @@ export default {
         },
 
         legend: {
+          enabled: true,
           layout: "horizontal",
           align: "center",
-          verticalAlign: "bottom"
+          verticalAlign: "bottom",
+          y: 0
         },
 
         plotOptions: {
@@ -182,12 +184,12 @@ export default {
           }
         },
         tooltip: {
-          headerFormat:
-            '<span style="font-size:10px">{point.key}</span><table>',
+          /* headerFormat:
+            '<span style="font-size:10px">{point.key}</span><table>', */
           pointFormat:
             '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
             '<td style="padding:0"><b>{point.y}</b></td></tr>',
-          footerFormat: "</table>",
+          /* footerFormat: "</table>", */
           shared: true,
           useHTML: true
         },
@@ -254,6 +256,17 @@ export default {
                       }
                     }
                   }
+                }
+              }
+            },
+            {
+              condition: {
+                maxWidth: 575
+              },
+
+              chartOptions: {
+                legend: {
+                  enabled: false
                 }
               }
             }
