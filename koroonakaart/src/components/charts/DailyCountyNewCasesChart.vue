2eb2dbb671da8ead42f6beaddce9ff7dce5bb9c9
sarnane <sarnane+github@pm.me>
2021-04-19 22:43:07 +0300

    Remove percentage

--- koroonakaart/src/components/charts/DailyCountyNewCasesChart.vue
+++ koroonakaart/src/components/charts/DailyCountyNewCasesChart.vue
@@ -156,7 +156,7 @@ export default {
             '<span style="font-size:10px">{point.key}</span><table>',
           pointFormat:
             '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y}</b> ({point.percentage:.0f}%)</td></tr>',
+            '<td style="padding:0"><b>{point.y}</b></td></tr>',
           split: false,
           useHTML: true,
         },
