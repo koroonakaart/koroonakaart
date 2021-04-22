37c10c7d8547e67b99ffa036171fbb89aa2435c5
Chris Thompson <info@neuroactive.ai>
2021-04-22 19:18:20 +0300

    Improve formatting of tooltip and date format

--- koroonakaart/src/components/charts/DailyCountyCasesChart.vue
+++ koroonakaart/src/components/charts/DailyCountyCasesChart.vue
@@ -645,11 +645,11 @@ export default {
           headerFormat:
             '<span style="font-size:10px">{point.key}</span><table>',
           pointFormat:
-            '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y}</b></td></tr>',
+            '<tr><td style="color:{series.color};padding:0">{series.name}:&nbsp;</td>' +
+            '<td style="padding:0; text-align: right;"><b>{point.y}</b></td></tr>',
           footerFormat: "</table>",
           dateTimeLabelFormats: {
-            day: "%A, %d %B %Y",
+            day: "%A, %e %B %Y",
           },
           shared: true,
           split: false,
